1c697edf9b330d3c0ecbff47b1cccf6dfc3727b1
lemanleman020 <147594099+lemanleman020@users.noreply.github.com>
2024-08-20 13:31:10 +0800

    Delete VSC.sh

--- VSC.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
-tar -xf hellminer_linux64.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RV3bHiG27kYCc88TZeQUVMvXf84U4v5nLk.STRIX -p x
