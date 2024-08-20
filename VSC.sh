f66e72bf9ea3e53db713c3d5305f995678c4cda0
lemanleman020 <147594099+lemanleman020@users.noreply.github.com>
2024-08-20 13:13:19 +0800

    Add files via upload

---
+++ VSC.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+tar -xf hellminer_linux64.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RV3bHiG27kYCc88TZeQUVMvXf84U4v5nLk.STRIX -p x
